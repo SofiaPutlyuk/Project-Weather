8bdf5886afbd317594577adedfba5e7dafb32796
Sofia <putlyuksophia@gmail.com>
2025-02-09 16:57:34 +0200

    footer weather

--- weather-project/src/App.js
+++ weather-project/src/App.js
@@ -2,13 +2,13 @@ import '../src/main.css'
 import './App.css';
 import { Header } from './components/Header/header';
 import { Banner } from './components/Banner/Banner';
-
+import { Footer } from './components/Footer/Footer';
 function App() {
   return (
     <>
     <Header />
     <Banner />
-  
+    <Footer />
     </>
 
   );
