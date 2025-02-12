f629bb2db39bce08f854d79a2a4641730ae8a7d5
Sofia <putlyuksophia@gmail.com>
2025-02-12 11:04:31 +0200

    card weather

--- weather-project/src/components/Banner/SearchInfo.jsx
+++ weather-project/src/components/Banner/SearchInfo.jsx
@@ -1,8 +1,10 @@
-const API_KEY = 'c899df01a007e998373f0576e8f261c7'
-export const SearchInfo =  () => {
-    const inputElement = document.querySelector('.banner-input')
-    const valueCity = inputElement.value
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${valueCity}&appid=${API_KEY}&units=metric`)
-    .then(responsive => responsive.json())
-        .then(info => console.log(info))
+import { Cards } from "../Cards/InfoCards";
+
+export const SearchInfo = ({ weather }) => {
+    return (
+        <div className="search-info">
+            {weather && <Cards weather={weather} />}
+        </div>
+    );
 }
+
